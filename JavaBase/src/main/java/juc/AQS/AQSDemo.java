c658971b778aec1675f0ec4ce4ee3cd02fe90c87
mike <123@qq.com>
2023-08-25 23:28:40 +0800

    juc

--- JavaBase/src/main/java/juc/AQS/AQSDemo.java
+++ JavaBase/src/main/java/juc/AQS/AQSDemo.java
@@ -5,13 +5,18 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class AQSDemo {
     public static void main(String[] args) {
-        Lock lock = new ReentrantLock();//参数默认为false，非公平锁
-
-        lock.lock();
-        try{
-
-        }finally {
-            lock.unlock();
-        }
+//        Lock lock = new ReentrantLock();//参数默认为false，非公平锁
+//
+//        lock.lock();
+//        try{
+//
+//        }finally {
+//            lock.unlock();
+//        }
+        int m=121;
+        int bai = m/100;
+        int shi = (m-100*bai)/10;
+        int ge = (m-100*bai-10*shi);
+        System.out.println(ge);
     }
 }
