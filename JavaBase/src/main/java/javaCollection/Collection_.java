9fd18b1f5a65039823228334fd781bf6ed3a5bc2
mike <123@qq.com>
2024-08-30 17:40:35 +0800

    review

--- JavaBase/src/main/java/javaCollection/Collection_.java
+++ JavaBase/src/main/java/javaCollection/Collection_.java
@@ -12,26 +12,26 @@ public class Collection_ {
         arrayList.add("aa");
 
         Collections.reverse(arrayList);
-        System.out.println("反转："+ arrayList);
+        System.out.println("鍙嶈浆锛�"+ arrayList);
         Collections.shuffle(arrayList);
-        System.out.println("洗牌："+ arrayList);
+        System.out.println("娲楃墝锛�"+ arrayList);
         Collections.sort(arrayList);
-//        也可自定义排序规则sort(List, Comparator)
-        System.out.println("排序："+ arrayList);
-//        也可自定义排序规则max(List, Comparator)
-        System.out.println("自然排序的最大元素："+ Collections.max(arrayList));
+//        涔熷彲鑷畾涔夋帓搴忚鍒檚ort(List, Comparator)
+        System.out.println("鎺掑簭锛�"+ arrayList);
+//        涔熷彲鑷畾涔夋帓搴忚鍒檓ax(List, Comparator)
+        System.out.println("鑷劧鎺掑簭鐨勬渶澶у厓绱狅細"+ Collections.max(arrayList));
         Collections.swap(arrayList,0,2);
-        System.out.println("调换："+ arrayList);
-        System.out.println("aa出现的频率："+Collections.frequency(arrayList,"aa"));
+        System.out.println("璋冩崲锛�"+ arrayList);
+        System.out.println("aa鍑虹幇鐨勯鐜囷細"+Collections.frequency(arrayList,"aa"));
         ArrayList arrayList1 = new ArrayList();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
             arrayList1.add("");
         }
         Collections.copy(arrayList1,arrayList);
-//        注意复制对象不能为空，且容必须大于等于被复制对象
-        System.out.println("复制："+arrayList1);
+//        娉ㄦ剰澶嶅埗瀵硅薄涓嶈兘涓虹┖锛屼笖瀹瑰繀椤诲ぇ浜庣瓑浜庤澶嶅埗瀵硅薄
+        System.out.println("澶嶅埗锛�"+arrayList1);
         Collections.replaceAll(arrayList,"aa","AA");
-        System.out.println("替换："+ arrayList);
+        System.out.println("鏇挎崲锛�"+ arrayList);
 
 
     }
